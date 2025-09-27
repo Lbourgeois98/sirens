9a5a337432bd12cf5b6c6c0556965b6d106b1c82
LeAnn <eyesseekreality0@gmail.com>
2025-09-26 22:26:42 -0600

    Convert Vercel serverless function to ES module syntax for Stripe Onramp

--- api/create-onramp-session.js
+++ api/create-onramp-session.js
@@ -1,19 +1,17 @@
-const Stripe = require('stripe');
+import Stripe from 'stripe';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method not allowed' });
     return;
   }
 
-  const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
   try {
-    const session = await stripe.crypto.onrampSessions.create({
-      // You can pass more parameters here if needed
-    });
+    const session = await stripe.crypto.onrampSessions.create({});
     res.status(200).json({ clientSecret: session.client_secret });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-};
+}
